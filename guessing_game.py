a2d066f4926ec260af3b0c446312e94e3ae18495
matyasi.zsofia <zsofia.matyasi@gmail.com>
2020-12-16 14:19:26 +0100

    Outsource game logic to two main functions

--- guessing_game.py
+++ guessing_game.py
@@ -1,51 +1,33 @@
-# a game where player has to guess 10 random numbers in range 1 to 99 
-# and 10 random numbers in range 1 to 49
-import math #imports math module
-import random #imports random module
-a = [] #initializes an empty list
-a.append(random.randint(1, 99)) # adds a random number (1-99) to the list
-a.append(random.randint(1, 99)) # adds a random number (1-99) to the list
-a.append(random.randint(1, 99)) # adds a random number (1-99) to the list
-a.append(random.randint(1, 99)) # adds a random number (1-99) to the list
-a.append(random.randint(1, 99)) # adds a random number (1-99) to the list
-a.append(random.randint(1, 99)) # adds a random number (1-99) to the list
-a.append(random.randint(1, 99)) # adds a random number (1-99) to the list
-a.append(random.randint(1, 99)) # adds a random number (1-99) to the list
-a.append(random.randint(1, 99)) # adds a random number (1-99) to the list
-a.append(random.randint(1, 99)) # adds a random number (1-99) to the list
-for i in range(10): # the process is done for every element of the list
-    g = int(input("Enter an integer from 1 to 99: ")) # player inputs the guess
-    while a[i] != g: # while guess is not the number to be guessed this process repeats
-        if g < a[i]: # if guess is lower than number to be guessed
-            print("guess is low") # prints "guess is low"
-            g = int(input("Enter an integer from 1 to 99: ")) # asks for another guess from the player
-        elif g > a[i]: # if guess is higher than number to be guessed
-            print("guess is high") # prints "guess is high"
-            g = int(input("Enter an integer from 1 to 99: ")) # asks for another guess from the player
-        else: # the remaining option is true (guess equals the number to be guessed)
-            break # breaks out of the cycle
-    print("you guessed it!") # plyaer is notofied that they guessed the number
+import random
 
-b = [] #initializes an empty list
-b.append(random.randint(1, 49)) # adds a random number (1-49) to the list
-b.append(random.randint(1, 49)) # adds a random number (1-49) to the list
-b.append(random.randint(1, 49)) # adds a random number (1-49) to the list
-b.append(random.randint(1, 49)) # adds a random number (1-49) to the list
-b.append(random.randint(1, 49)) # adds a random number (1-49) to the list
-b.append(random.randint(1, 49)) # adds a random number (1-49) to the list
-b.append(random.randint(1, 49)) # adds a random number (1-49) to the list
-b.append(random.randint(1, 49)) # adds a random number (1-49) to the list
-b.append(random.randint(1, 49)) # adds a random number (1-49) to the list
-b.append(random.randint(1, 49)) # adds a random number (1-49) to the list
-for i in range(10): # the process is done for every element of the list
-    g = int(input("Enter an integer from 1 to 49: ")) # player inputs the guess
-    while b[i] != g: # while guess is not the number to be guessed this process repeats
-        if g < b[i]: # if guess is lower than number to be guessed
-            print("guess is low") # prints "guess is low"
-            g = int(input("Enter an integer from 1 to 49: ")) # asks for another guess from the player
-        elif g > b[i]: # if guess is higher than number to be guessed
-            print("guess is high") # prints "guess is high"
-            g = int(input("Enter an integer from 1 to 49: ")) # asks for another guess from the player
-        else: # the remaining option is true (guess equals the number to be guessed)
-            break #breaks out of the cycle
-    print("you guessed it!") # plyaer is notofied that they guessed the number
+
+def process_game(length_of_list, start_range, end_range):
+    list_of_numbers = create_list_of_random_numbers(length_of_list, start_range, end_range)
+    play_guessing_game(list_of_numbers, start_range, end_range)
+
+
+def create_list_of_random_numbers(length_of_list, start_range, end_range):
+    list_of_random_numbers = []
+    for i in range(length_of_list):
+        list_of_random_numbers.append(random.randint(start_range, end_range))
+    return list_of_random_numbers
+
+
+def play_guessing_game(list_of_numbers, start_range, end_range):
+    for number in list_of_numbers:
+        guess = int(input(f"Enter an integer from {start_range} to {end_range}: "))
+        while number != guess:
+            if guess < number:
+                print("guess is low")
+                guess = int(input("Enter an integer from 1 to 99: "))
+            elif guess > number:
+                print("guess is high")
+                guess = int(input("Enter an integer from 1 to 99: "))
+            else:
+                break
+        print("you guessed it!")
+
+
+if __name__ == "__main__":
+    process_game(10, 1, 99)
+    process_game(10, 1, 49)
